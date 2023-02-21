d3420ec3639347100b79e818fdb7ebda7bbeddc7
Manoj0634 <msrmj01@gmail.com>
2023-02-21 14:14:44 +0530

    Create stock_price.py

---
+++ stock_price.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import streamlit as st 
+import sklearn
+import yfinance as yf 
+import numpy as np
+import datetime
+#for heading styles we can refer to  stremlit api reference
+st.write(
+    """
+    # Stock Price Analyzer
+    Shown are the stock prices of Apple.
+    """
+)
+
+ticker_symbol=st.text_input("Enter the Stock Symbol","AAPL",key="placeholder")
+title=st.text_input
+col1,col2=st.columns(2)
+with col1:
+    start_date=st.date_input("Input starting data", 
+               datetime.date(2019,1,1))
+with col2:
+    end_date=st.date_input("Input end date",datetime.date(2022,12,31)) 
+
+
+st.write(f"""### {ticker_symbol}""")
+
+
+#ticker_symbol ="anything"
+ticker_data=yf.Ticker(ticker_symbol)
+ticker_df=ticker_data.history(period="1d",start=f"{start_date}",end=f"{end_date}")
+#creating a table 
+st.dataframe(ticker_df)
+st.write(""" 
+## Daily closing price chart
+""")
+#creating line chart with only close values 
+st.line_chart(ticker_df.Close)
+st.write(""" 
+## Volume of shares traded each day
+""")
+#creating line chart with only volume values 
+st.line_chart(ticker_df.Volume)
